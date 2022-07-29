769f12aeac53ac0124d5fd662913b539322182e2
Axel-Padilla <apadillac1700@alumno.ipn.mx>
2022-07-29 01:06:39 -0500

    add: exercise 2.2 and ternary operator example

--- S2.js
+++ S2.js
@@ -14,3 +14,51 @@ if (time >= 0 && time < 12){
 }
 
 console.log(greeting);
+
+// Exercise 2
+
+const day = 5;
+let text;
+
+switch (day) {
+    case 1:
+        text = 'Monday'
+        break;
+    case 2:
+        text = 'Tuesday'
+        break;
+    case 3:
+        text = 'Wednesday'
+        break;
+    case 4:
+        text = 'Thursday'
+        break;
+    case 5:
+        text = 'Friday'
+        break;
+    case 6:
+        text = 'Saturday'
+        break;
+    case 7:
+        text = 'Sunday'
+        break;
+    default:
+        text = 'Your day number doesn\'t exist'
+        break;
+}
+console.log(text);
+
+// Ternary operator example
+
+const speed = 90;
+let message;
+
+message = speed > 100 ? 'You\'re going very fast' : 'You\'re going at the right speed';
+// if (speed > 100) {
+//     message = 'You\'re going very fast'
+// } else {
+//     message = 'You\'re going at the right speed'
+// }
+console.log(message);
+const isFast = speed > 100 ? true : false;
+console.log('Are you really going so fast?', speed, 'km/h,', isFast);
