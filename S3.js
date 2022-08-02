cccfe796895dce7b3e40ea41c2babfa65840a0b2
Axel-Padilla <apadillac1700@alumno.ipn.mx>
2022-08-01 22:15:48 -0500

    add: example 2 and challenge 1, using of for-of

--- S3.js
+++ S3.js
@@ -1,11 +1,36 @@
 // Session 3 - Example 1
 
-const numbers = [1, 3, 4, 7, 2, 1, 9, 0]
-// RESULT: const doubled = [2, 6, 8, 14, 4, 2, 18, 0]
-const doubled = [];
-for (let index = 0; index < numbers.length; index++) {
-    doubled.push(numbers[index] * 2);
-}
+// const numbers = [1, 3, 4, 7, 2, 1, 9, 0]
+// // RESULT: const doubled = [2, 6, 8, 14, 4, 2, 18, 0]
+// const doubled = [];
+// for (let index = 0; index < numbers.length; index++) {
+//     doubled.push(numbers[index] * 2);
+// }
 
-console.log('Numbers:', numbers)
-console.log('Doubled:', doubled)
+// console.log('Numbers:', numbers)
+// console.log('Doubled:', doubled) 
+
+// Example 2
+
+// const numbers = [1, 3, 4, 7, 2, 1, 9, 0]
+// const doubled = [];
+// for (const number of numbers) {
+//     doubled.push(number * 2);
+// }
+
+// console.log('Numbers:', numbers)
+// console.log('Doubled:', doubled) 
+
+// CHALLENGE 1 - for the given array, get the average
+
+const numbers = [5, 3, 4, 7, 2, 1, 9, 7, 7];
+let sum = 0;
+for (const num of numbers){
+    sum = num + sum
+}
+const average = sum / numbers.length;
+console.log('The average of the array\'s elements is:', average);
+// Alternative solution
+// for (let index = 0; index < numbers.length; index++) {
+//     sum = sum + numbers[index];
+// }
