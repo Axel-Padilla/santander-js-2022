1efa43269f16cd367bf69819d12ef7d7b8bb2e12
Axel-Padilla <apadillac1700@alumno.ipn.mx>
2022-08-03 15:36:16 -0500

    add: challenge 1, function power

--- S4.js
+++ S4.js
@@ -10,6 +10,18 @@ function yearsUntilRetirement (yearOfBirth, name) {
     const age = calculateAge(yearOfBirth);
     const retirement = 65 - age;
     console.log(`${name} retires in ${retirement} years`);
-    console.log(name + ' retires in ' + retirement + ' years');
+    // console.log(name + ' retires in ' + retirement + ' years');
 }
 yearsUntilRetirement(1969, 'John')
+
+// CHALLENGE 1 - create a function to calculate the power of a number
+
+function power (base, exponent) {
+    let sum = 1;
+    for (let i = 0; i < exponent; i++) {
+        sum = sum * base
+    }
+    return sum;
+}
+const result = power(3,0)
+console.log(result)
