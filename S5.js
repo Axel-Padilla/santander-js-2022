67058d1cd926d810a835f76014f511baf3f78ff9
Axel-Padilla <apadillac1700@alumno.ipn.mx>
2022-08-14 00:20:29 -0500

    add: diference between function expressions and declarations

--- S5.js
+++ S5.js
@@ -25,6 +25,8 @@
 // })()
 // console.log(foo)
 
+// THIS EXAMPLES
+
 // function logThis () {
 //     console.log("Is this === window?" + (this === window) )
 // }
@@ -36,16 +38,44 @@
 // logThis() // Is this === window? true 
 // foo.logThis() // Is this === window? false
 
-const john = {
-    firstName: 'John',
-    lastName: 'Doe',
-    birthYear: 1990,
-    calculateAge: function() {
-        const today = new Date()
-        const year = today.getFullYear()
-        this.age = year - this.birthYear
+// const john = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     birthYear: 1990,
+//     calculateAge: function() {
+//         const today = new Date()
+//         const year = today.getFullYear()
+//         this.age = year - this.birthYear
+//     }
+// }
+// console.log(john)
+// john.calculateAge()
+// console.log(john)
+
+/* It's not the same to use function expressions or function declarations, because 
+    of the hoisting. While in declarations the function is hoisted (which means that we can 
+    access even before initialize the function), in expressions we need to initialize at first. 
+    NOTE: this happens with function calls, necessarily*/
+
+console.log(power(2,2)) // We get 4
+function power (base, exponent) {
+    let sum = 1;
+    for (let i = 0; i < exponent; i++) {
+        sum = sum * base
     }
+    return sum;
 }
-console.log(john)
-john.calculateAge()
-console.log(john)
+console.log(power(2,2)) // We get 4
+
+// Function expression (arrow function)
+console.log(power1(2,2)) // We cannot access before initializing
+const power1 = (base, exponent) => {
+    let sum = 1;
+    for (let i = 0; i < exponent; i++) {
+        sum = sum * base
+    }
+    return sum;
+}
+console.log(power1(2,2)) // We get 4
+
+
